0556d701653c3674b0ae96aa4f468b5decf8187c
vamsics2 <39087062+vamsics2@users.noreply.github.com>
2021-07-07 12:37:04 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,5 @@ sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo chmod 666 /var/run/docker.sock
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+#startingdocker
 sudo service docker start
